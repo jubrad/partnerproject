f744640f43de9d68b4fd6304adaf16f93f8f3e4f
jubrad <bradfield.justin7@gmail.com>
2013-12-03 18:42:39 -0500

    the Ifram no longer appears for https sites

--- scripts/background.js
+++ scripts/background.js
@@ -1,6 +1,22 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
 	chrome.tabs.executeScript(
 		tab.id, {
+		file: "resources/jquery.min.js"
+	});
+});
+
+
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+	chrome.tabs.executeScript(
+		tab.id, {
 		file: "scripts/toolbar.js"
+	});
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+	chrome.tabs.executeScript(
+		tab.id, {
+		code: "console.log('linkshare button clicked')"
 	});
 });
--- scripts/toolbar.js
+++ scripts/toolbar.js
@@ -1,8 +1,9 @@
 
 function setupLinkBar(){
-	if($('#linkshare-toolbar').length  == 0 ){
-		alert('adding an iframe');
-		$('body').prepend('<iframe id="linkshare-toolbar" src="www.leekspin.com" width="100%>"" height="200"></iframe>');
+	console.log('setupLinkBar() has been called')
+	if($('#linkshare-toolbar').length  == 0 && document.location.protocol != "https:"){
+		$('body').prepend('<iframe id="linkshare-toolbar" style="z-index:100 " src="http://www.leekspin.com" width="100%" height="100"></iframe>');
+		// $('linkshare-toobar').
 	}
 	else{
 		$('#linkshare-toolbar').remove();
