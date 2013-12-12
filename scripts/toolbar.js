119057f2be3f58af335a969c25942c9a25229f4b
jubrad <bradfield.justin7@gmail.com>
2013-12-12 10:27:25 -0500

    basic changes to layout on toolbar

--- scripts/toolbar.js
+++ scripts/toolbar.js
@@ -1,12 +1,11 @@
-
 function setupLinkBar(){
 	console.log('setupLinkBar() has been called')
 	if($('#linkshare-toolbar').length  == 0 && document.location.protocol != "https:"){
 
 		link = $(location).attr('href');
 
-		$('body').prepend("<iframe id=\"linkshare-toolbar\" src=\"http://linkshare-burstworks.herokuapp.com/shares/new?url=" + link + "\" seamless></iframe>");
-		$('#linkshare-toolbar').css({'position':'fixed','z-index':'10000','width':'100%','height':'0px','top': '0px' ,'left': '0px ','right':'0px ','overflow':'hidden ','margin-top':'0px','padding-top':'0px'});
+		$('body').prepend("<iframe id=\"linkshare-toolbar\" src=\"http://localhost:3000/shares/new?url=" + link + "\" seamless></iframe>");
+		$('#linkshare-toolbar').css({'position':'fixed','box-shadow': '0 1px 9px black','z-index':'999999999','width':'100%','height':'0px','top': '0px' ,'left': '0px ','right':'0px ','overflow':'hidden ','margin-top':'0px','padding-top':'0px'});
 		$('#linkshare-toolbar').animate({'height': '100px'},300);
 	}
 	else{
@@ -19,7 +18,4 @@ function setupLinkBar(){
 
 setupLinkBar();
 
-// 'width': "" + $(document).width()})
 
-
-		// $('body').prepend("<iframe id=\"linkshare-toolbar\" src=\"www.google.com/?url=\" " + pathname +" seamless></iframe>");
