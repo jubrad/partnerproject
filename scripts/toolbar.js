3e2159c6656951b4000d060ff73997ce5b990edb
toppy42 <toppy42@gmail.com>
2013-12-12 09:14:23 -0500

    increased Z-index

--- scripts/toolbar.js
+++ scripts/toolbar.js
@@ -4,9 +4,9 @@ function setupLinkBar(){
 	if($('#linkshare-toolbar').length  == 0 && document.location.protocol != "https:"){
 
 		link = $(location).attr('href');
-
-		$('body').prepend("<iframe id=\"linkshare-toolbar\" src=\"http://linkshare-burstworks.herokuapp.com/shares/new?url=" + link + "\" seamless></iframe>");
-		$('#linkshare-toolbar').css({'position':'fixed','z-index':'10000','width':'100%','height':'0px','top': '0px' ,'left': '0px ','right':'0px ','overflow':'hidden ','margin-top':'0px','padding-top':'0px'});
+		/*Correct URL: http://linkshare-burstworks.herokuapp.com/shares/new?url=*/
+		$('body').prepend("<iframe id=\"linkshare-toolbar\" src=\"http://localhost:3000/shares/new?url=" + link + "\" seamless></iframe>");
+		$('#linkshare-toolbar').css({'position':'fixed','z-index':'999999','width':'100%','height':'0px','top': '0px' ,'left': '0px ','right':'0px ','overflow':'hidden ','margin-top':'0px','padding-top':'0px'});
 		$('#linkshare-toolbar').animate({'height': '100px'},300);
 	}
 	else{
